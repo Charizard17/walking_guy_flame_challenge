03aa210a4ec58ad34234c29e4b4be7458ec985ea
Esad Dursun <3723esad@gmail.com>
2022-01-15 22:03:55 +0100

    non-moving sprite and animated sprite knights displayed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 
-import './walking_knight.dart';
+import 'walking_knight_game.dart';
 
 main() {
-  final game = WalkingKnight();
+  final game = WalkingKnightGame();
   runApp(
     GameWidget(game: game),
   );
---
+++ lib/walking_knight_game.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+
+class WalkingKnightGame extends FlameGame {
+  SpriteComponent knight = SpriteComponent();
+  SpriteAnimationComponent walkingKnight = SpriteAnimationComponent();
+  String direction = 'right';
+  double walkingSpeed = 1;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    knight
+      ..sprite = await loadSprite('knight_stand.png')
+      ..size = Vector2(58.7, 70.7)
+      ..x = 150
+      ..y = 250;
+    add(knight);
+
+    var spriteSheet = await images.load('knight_spritesheet.png');
+    final spriteSize = Vector2(58.7, 70.7);
+    SpriteAnimationData spriteData = SpriteAnimationData.sequenced(
+        amount: 10,
+        stepTime: 0.1,
+        textureSize: Vector2(100 / 70.7 * 58.7, 100));
+    walkingKnight =
+        SpriteAnimationComponent.fromFrameData(spriteSheet, spriteData)
+          ..x = 5
+          ..y = 105
+          ..size = spriteSize;
+
+    add(walkingKnight);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    switch (direction) {
+      case 'right':
+        walkingKnight.x += walkingSpeed;
+        break;
+      case 'down':
+        walkingKnight.y += walkingSpeed;
+        break;
+      case 'left':
+        walkingKnight.x -= walkingSpeed;
+        break;
+      case 'up':
+        walkingKnight.y -= walkingSpeed;
+        break;
+    }
+
+    if (walkingKnight.x > 300) {
+      direction = 'down';
+    }
+    if (walkingKnight.y > 500) {
+      direction = 'left';
+    }
+    if (walkingKnight.x < 5) {
+      direction = 'up';
+    }
+    if (walkingKnight.y < 105) {
+      direction = 'right';
+    }
+  }
+}
