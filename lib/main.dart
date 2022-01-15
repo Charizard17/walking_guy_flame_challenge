fb3c2b248fd51284c35f4af3297719f76f3c2afb
Esad Dursun <3723esad@gmail.com>
2022-01-15 20:58:25 +0100

    flame installed. main file edited. walking_knight file created. show knight image

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+
+import './walking_knight.dart';
+
+main() {
+  final game = WalkingKnight();
+  runApp(
+    GameWidget(game: game),
+  );
+}
